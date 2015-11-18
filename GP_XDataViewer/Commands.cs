ff9686e9d9813012d41a92bd5730075559044ce7
Vildar Khisyametdinov <vildar82@gmail.com>
2015-11-18 15:48:19 +0300

    1

---
+++ GP_XDataViewer/Commands.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using GP_XDataViewer.Model.XDataViewer;
+
+[assembly: CommandClass(typeof(GP_XDataViewer.Commands))]
+
+namespace GP_XDataViewer
+{
+   public class Commands
+   {
+      [CommandMethod("PIK", "GP-XDataShow", CommandFlags.Modal)]
+      public void XDataShowCommand()
+      {
+         var doc = Application.DocumentManager.MdiActiveDocument;
+         if (doc == null) return;
+         Editor ed = doc.Editor;
+
+         var opt = new PromptEntityOptions("Выбери приметив:");
+         var res = ed.GetEntity(opt);
+         if (res.Status == PromptStatus.OK)
+         {
+            TypedValue[] typedValues = null;
+            string entName = string.Empty;
+            using (var ent = res.ObjectId.Open(OpenMode.ForRead) as Entity)
+            {
+               if (ent.XData == null)
+               {
+                  ed.WriteMessage("\nНет расширенных данных у {0}", ent);
+                  return;
+               }
+               else
+               {
+                  typedValues = ent.XData.AsArray();
+                  entName = ent.ToString();
+               }
+            }            
+            FormXDataView formXdataView = new FormXDataView(typedValues, entName);
+            Application.ShowModalDialog(formXdataView);
+         }
+      }
+   }
+}
---
+++ GP_XDataViewer/Model/XDataViewer/FormXDataView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace GP_XDataViewer.Model.XDataViewer
+{
+   public partial class FormXDataView : Form
+   {
+      public FormXDataView(TypedValue[] typedValues, string entName)
+      {
+         InitializeComponent();
+         label1.Text = entName;
+         richTextBox1.Text = getXdataAllText(typedValues);         
+      }
+
+      private string getXdataAllText(TypedValue[] typedValues)
+      {
+         StringBuilder sbText = new StringBuilder();
+         foreach (var item in typedValues)
+         {
+            sbText.AppendLine(string.Format("TypeCode: {0}; Value: {1}", item.TypeCode, item.Value));
+         }
+         return sbText.ToString();
+      }
+   }
+}
